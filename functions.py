47bb6f74fd32c98e167ee6ca0d207e7fa52a07ac
Ryan Lee <rjlguitar@gmail.com>
2020-09-04 21:41:59 -0700

    functions tutorial

---
+++ functions.py
@@ -0,0 +1,39 @@
+def area(width, height):
+    result = width * height
+    return result
+    
+result = area(5, 6)
+print(result)
+result = area(7, 8)
+print(result)
+result = area(9, 10)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result = subtract(5, 4)
+print(result)
+result = subtract(9, 6)
+print(result)
+
+def divide(num3, num4):
+    result = num3 / num4
+    return result
+
+result = divide(20, 5)
+print(result)
+result = divide(30, 6)
+print(result)
+
+def add(num5, num6):
+    result = num5 + num6
+    return result
+
+result = add(4, 7)
+print(result)
+result = add(7, 9)
+print(result)
+result = add(3, 8)
+print(result)
